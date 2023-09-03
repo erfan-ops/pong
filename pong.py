b965ca595f17624014e36d5a218edae9ca61f9d4
Erfan Karami <erfan012amir016@gmail.com>
2023-09-03 04:32:06 +0430

    modified:   pong.py modified:   screen.py

--- pong.py
+++ pong.py
@@ -52,30 +52,6 @@ class Game(Screen):
     def get_fps(self):
         self.FPS = self.clock.get_fps() if self.clock.get_fps() > 30 else float(self._REFRESH_RATE)
     
-    
-    def start_screen(self) -> None:
-        loading_img = pygame.image.load("assets/loading_screen.png").convert()
-        size = loading_img.get_size()
-        width_ratio = SCREEN_WIDTH/size[0]
-        loading_img = pygame.transform.rotozoom(loading_img, 0, width_ratio)
-        loading_img_rect = loading_img.get_rect()
-        loading_line_width = 0
-        space_from_edge = SCREEN_WIDTH/12
-        line_rect = [space_from_edge, SCREEN_HEIGHT-SCREEN_HEIGHT/8, loading_line_width, 20]
-        self.screen.fill("#000000")
-        self.screen.blit(loading_img, loading_img_rect)
-        pygame.display.flip()
-        while True:
-            self.check_game_quit()
-            self.get_fps()
-            pygame.draw.rect(self.screen, "#ffffff", line_rect)
-            line_rect[2] += 360*width_ratio/self.FPS
-            pygame.display.update(line_rect)
-            self.clock.tick(self.FPS)
-            if line_rect[2] >= SCREEN_WIDTH-(space_from_edge)*2:
-                del loading_img
-                return None
-    
 
     def main(self) -> None:
         pygame.mouse.set_visible(False)
--- screen.py
+++ screen.py
@@ -13,3 +13,27 @@ class Screen:
             self.screen.fill(self.bg_color)
             for obj in args:
                 self.screen.blit(obj.img, obj.rect)
+    
+    
+    def start_screen(self) -> None:
+        loading_img = pygame.image.load("assets/loading_screen.png").convert()
+        size = loading_img.get_size()
+        width_ratio = SCREEN_WIDTH/size[0]
+        loading_img = pygame.transform.rotozoom(loading_img, 0, width_ratio)
+        loading_img_rect = loading_img.get_rect()
+        loading_line_width = 0
+        space_from_edge = SCREEN_WIDTH/12
+        line_rect = [space_from_edge, SCREEN_HEIGHT-SCREEN_HEIGHT/8, loading_line_width, 20]
+        self.screen.fill("#000000")
+        self.screen.blit(loading_img, loading_img_rect)
+        pygame.display.flip()
+        while True:
+            self.check_game_quit()
+            self.get_fps()
+            pygame.draw.rect(self.screen, "#ffffff", line_rect)
+            line_rect[2] += 360*width_ratio/self.FPS
+            pygame.display.update(line_rect)
+            self.clock.tick(self.FPS)
+            if line_rect[2] >= SCREEN_WIDTH-(space_from_edge)*2:
+                del loading_img
+                return None

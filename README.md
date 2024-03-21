32315713543355f419f41063b724e1e7d8c86f06
Erfan Karami <erfan012amir016@gmail.com>
2024-03-22 00:44:16 +0330

    add exmaples

--- README.md
+++ README.md
@@ -8,4 +8,10 @@ pip install -U pyinstaller
 pyinstaller -F -w -i icon.ico pong.py
 ```
 
+
+<p align="center">
+  <img src="https://github.com/erfan-ops/pong/blob/main/examples/example.png" width=350 />
+</p>
+
+
 - btw the loading screen is unnecessary its not loading anything
--- settings.py
+++ settings.py
@@ -1,15 +1,20 @@
-SCREEN_WIDTH = 1600
-SCREEN_HEIGHT = 900
+from json import load
+with open("settings.json", "r") as f:
+    _SETTINGS = load(f)
+
+
+SCREEN_WIDTH = _SETTINGS["screen-width"]
+SCREEN_HEIGHT = _SETTINGS["screen-height"]
 RES = (SCREEN_WIDTH, SCREEN_HEIGHT)
-BG_COLOR = "#000000"
+BG_COLOR = _SETTINGS["bg-color"]
 
-P_SPEED = 20
-P_SIZE = 150
+P_SPEED = _SETTINGS["player-speed"]
+P_SIZE = _SETTINGS["player-size"]
 
-O_SPEED = 20
-O_SIZE = 180
+O_SPEED = _SETTINGS["opponent-speed"]
+O_SIZE = _SETTINGS["opponent-size"]
 
-PADDLE_SPACING = 100
+PADDLE_SPACING = _SETTINGS["paddle-space-from-screen"]
 
-BALL_SIZE = 70
-BALL_SPEED = 15
+BALL_SIZE = _SETTINGS["ball-size"]
+BALL_SPEED = _SETTINGS["ball-speed"]
